891e5320872a437bd83c27f5ec6f9c5659eb8db5
Ghulam Ishaq <g.ishaq2007@gmail.com>
2024-12-29 05:31:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+// let a = +prompt("enter your number ");
+// let b = +prompt("enter your friend value ");
+// let c = +prompt("add my number" );
+// let d = +prompt("enter charity value ");
+
+
+// let sum =(a+b+c) -d -b;
+// console.log (sum);
+
+
+
+
+
+var a = +prompt("enter first value in number");
+
+var b = prompt(" what do you want (+, -, x, / ,%)");
+var c = +prompt(" enter second value in number");
+
+
+if (b === "+") {
+    alert(a + c);
+    console.log(a + " " + c)   }
+    
+    
+    
+    
+    
+    else if (b === "-") {
+    alert(a - c);
+    console.log(a-c)
+ } else if (b === "x") {
+    alert(a * c);
+    console.log(a*c)
+ }
+ else if (b === "/") {
+    alert(a / c);
+    console.log(a/c)
+ } 
+
+ else if ( b === "%") {
+    alert(a % c);
+ console. log (a % c)
+ }
+
+
+
+
+
+
+
+
